9dc4f4d8178f36f26f2cd196410992e74e5e7749
ronfor101 <ronfor101@gmail.com>
2022-02-16 17:36:21 +0200

    added game info icons
    added health icons and ship level icons in multiplayer :)

--- src/chicken_invaders/Client/GameInfo.java
+++ src/chicken_invaders/Client/GameInfo.java
@@ -16,9 +16,20 @@ public class GameInfo extends JPanel
     int width;
     int height;
     public Image enemyScreen;
+    Image background;
+    Image heart;
+    Image emptyHeart;
+    Image upgrade;
+    int iconSize;
     
     public GameInfo(GameManager game)
     {
+        background = (new ImageIcon("GameInfoBackground.jpg")).getImage();
+        heart = (new ImageIcon("Heart.png")).getImage();
+        emptyHeart = (new ImageIcon("EmptyHeart.png")).getImage();
+        upgrade = (new ImageIcon("Upgrade.png")).getImage();
+        iconSize = 50;
+        
         this.game = game;
         width = 512;
         height = 350;
@@ -27,24 +38,32 @@ public class GameInfo extends JPanel
         enemyScreen = (new ImageIcon("placeholder.png")).getImage();
     }
     
-    private void isEnemyData()
-    {
-//        if (game.enemyData.player != game.player) 
-//        {
-//            enemyScreen = game.enemyData.screen.getImage();
-//        }
-    }
-    
     public void paintComponent(Graphics g)	
     {
 	super.paintComponent(g);
-        isEnemyData();
-        g.drawImage(enemyScreen,0,height,width,height,null);
+        g.drawImage(background,0,0,getWidth(),getHeight(),null);
+        
+        for (int i = 0; i < game.lives; i++) 
+        {
+            g.drawImage(heart,((height / 2) - 50) + (i * 50), (height / 5), iconSize, iconSize, null);
+        }
+        
+        for (int i = 0; i < (3 - game.lives); i++) 
+        {
+            g.drawImage(emptyHeart,((height / 2) - 50) + ((game.lives + i) * 50), (height / 5), iconSize, iconSize, null);
+        }
+        
+        for (int i = 0; i < game.ship.shipLevel; i++) 
+        {
+            g.drawImage(upgrade,(height / 2) + (i * 55), (height / 5) + 85, iconSize, iconSize, null);
+        }
+        
+        //g.drawImage(enemyScreen,0,height,width,height,null);
         
         g.setColor(Color.white);
         g.setFont(new Font("Arial", 1, 25));
-        g.drawString("Health: " + game.lives, (height / 2) - 50, height / 5);
-        g.drawString("Ship Level: " + game.ship.shipLevel, (height / 2) - 50, (height / 5) * 2);
+        g.drawString("Health: ", 30, (height / 5) + 30);
+        g.drawString("Ship Level: ", 30, (height / 5) + 120);
         
         repaint();
     }
